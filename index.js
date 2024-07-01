cf18dc4e1a06beda3927d4600a8fdca5681e875a
Abhav Goel <goelabhav2002@gmail.com>
2024-07-01 17:09:30 +0530

    url shortner api service

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 8001;
+
+app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
+
+
+
+
